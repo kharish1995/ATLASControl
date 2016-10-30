abb969be48763286459e6cb9f5769e11d8975022
HarishKarunakaran <karunaha73925@gmail.com>
2016-10-30 15:46:38 -0400

    Updated our package

--- ATLAS/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harish/ATLAS/src
-# Build directory: /home/harish/ATLAS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(ros/moveit)
--- ATLAS/build/ros/moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harish/ATLAS/src/ros/moveit
-# Build directory: /home/harish/ATLAS/build/ros/moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
