280890c0a36a830b3cb0c36aafc3e8b5675a7863
HarishKarunakaran <karunaha73925@gmail.com>
2016-10-30 15:36:11 -0400

    Update our workspace to start writing code

---
+++ ATLAS/build/ros/moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/harish/ATLAS/src/ros/moveit
+# Build directory: /home/harish/ATLAS/build/ros/moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
